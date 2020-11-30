34f881d6d82e87975304241bf962b5bf8e5499eb
Tom Chen <ctoicqtao@msn.com>
2020-11-30 19:03:24 +0800

    axios/vconsole 的增加

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VConsole from 'vconsole';
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
 
 Vue.config.productionTip = false
 
 Vue.use(ViewUI);
+
+//打开下面注释，则启用vConsole
+// const vConsole = new VConsole()
+// console.log(vConsole.version)
 
 /* eslint-disable no-new */
 new Vue({
